a21bdec51256cec03771a27bbf4f268e90aafea5
Nicola Erario <nicola.erario@gmail.com>
2023-12-29 18:04:32 +0100

    feat: Scaffold form for snippet creation

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,3 +1,43 @@
 export default function SnippetCreatePage() {
-  return <div className="font-bold">Create New Snippet</div>;
+  return (
+    <form action="">
+      <h3 className="p-3 font-bold">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label
+            className="w-12"
+            htmlFor="title"
+          >
+            Title
+          </label>
+          <input
+            id="title"
+            type="text"
+            name="title"
+            className="w-full rounded border p-2"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <label
+            className="w-12"
+            htmlFor="code"
+          >
+            Code
+          </label>
+          <textarea
+            id="code"
+            name="code"
+            className="w-full rounded border p-2"
+          />
+        </div>
+        <button
+          type="submit"
+          className="rounded bg-blue-200 p-2"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
 }
