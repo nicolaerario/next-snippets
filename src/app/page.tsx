fa06325bef371e31c947da0745d2902d556c3726
Nicola Erario <nicola.erario@gmail.com>
2023-12-30 14:02:47 +0100

    feat: Add list of snippets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Home page</div>;
+import { db } from '@/db';
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  return (
+    <div>
+      {snippets.map((snippet) => (
+        <li key={snippet.id}>{snippet.title}</li>
+      ))}
+    </div>
+  );
 }
