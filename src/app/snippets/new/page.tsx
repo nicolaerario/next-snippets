bf185ddd8c8107d2533e47d5ac602388c44ebcb8
Nicola Erario <nicola.erario@gmail.com>
2023-12-30 14:01:06 +0100

    feat: Add server function to create a snippet

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,6 +1,25 @@
+import { db } from '@/db';
+import { redirect } from 'next/navigation';
+
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    'use server';
+
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
+
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    redirect('/');
+  }
+
   return (
-    <form action="">
+    <form action={createSnippet}>
       <h3 className="p-3 font-bold">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
