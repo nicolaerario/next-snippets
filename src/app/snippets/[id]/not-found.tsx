ae275464903f004afc68770d931330fcb2b41bef
Nicola Erario <nicola.erario@gmail.com>
2023-12-30 14:02:22 +0100

    feat: Add dynamic route for single snippet
    Add loading and not found custom pages

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function SnippetNotFound() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Snippet not found!</h1>
+    </div>
+  );
+}
---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,18 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+
+interface Props {
+  params: { id: string };
+}
+
+export default async function ShowSnippetPage({ params }: Props) {
+  const snippet = await db.snippet.findUnique({
+    where: { id: Number(params.id) },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>{snippet.title}</div>;
+}
