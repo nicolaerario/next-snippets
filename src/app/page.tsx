ff23724c624aab18bb523ce363be348fcef7eeae
Nicola Erario <nicola.erario@gmail.com>
2023-12-31 18:59:21 +0100

    feat: Add view links and add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
-import { db } from '@/db';
+import db from '@/db';
+import Link from 'next/link';
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   return (
-    <div>
+    <div className="flex flex-col gap-2">
+      {/* Header */}
+      <div className="flex items-center justify-between py-2">
+        <h3 className="text-3xl font-bold">Snippets</h3>
+        <Link
+          href={'/snippets/new'}
+          className="rounded border p-2"
+        >
+          New
+        </Link>
+      </div>
+      {/* List of snippets */}
       {snippets.map((snippet) => (
-        <li key={snippet.id}>{snippet.title}</li>
+        <Link
+          key={snippet.id}
+          href={`/snippets/${snippet.id}`}
+          className="flex items-center justify-between rounded border p-2"
+        >
+          {snippet.title}
+          <span>View</span>
+        </Link>
       ))}
     </div>
   );
