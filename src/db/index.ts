46826830b916306f2826a531dd873e54bff88816
Nicola Erario <nicola.erario@gmail.com>
2023-12-31 19:06:22 +0100

    fix: Assignment

--- src/db/index.ts
+++ src/db/index.ts
@@ -12,7 +12,7 @@ const db = globalThis.prisma ?? prismaClientSingleton();
 
 export default db;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
 
 // NOTE: Best practice for instantiating PrismaClient with Next.js
 // https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
