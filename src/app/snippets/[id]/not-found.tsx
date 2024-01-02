bda0949283a5c6a0d8896d571886246f43db4c5b
Nicola Erario <nicola.erario@gmail.com>
2024-01-02 10:51:49 +0100

    style: Display message on the page center

--- src/app/snippets/[id]/not-found.tsx
+++ src/app/snippets/[id]/not-found.tsx
@@ -1,6 +1,6 @@
 export default function SnippetNotFound() {
   return (
-    <div>
+    <div className="flex h-screen items-center justify-center">
       <h1 className="text-3xl font-bold">Snippet not found!</h1>
     </div>
   );
