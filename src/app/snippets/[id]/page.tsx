b80ea1efe2efee84184b9e95c972bc7fd5d89ad4
Nicola Erario <nicola.erario@gmail.com>
2024-01-03 17:12:18 +0100

    feat: Add edit and delete actions

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,4 +1,6 @@
+import { deleteSnippet } from '@/actions';
 import db from '@/db';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 interface Props {
@@ -14,5 +16,27 @@ export default async function ShowSnippetPage({ params }: Props) {
     return notFound();
   }
 
-  return <div>{snippet.title}</div>;
+  const deleteSnippetAction = deleteSnippet.bind(null, snippet.id);
+
+  return (
+    <div>
+      <div className="my-4 flex items-center justify-between">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <Link
+            className="rounded border p-2"
+            href={`/snippets/${snippet.id}/edit`}
+          >
+            Edit
+          </Link>
+          <form action={deleteSnippetAction}>
+            <button className="rounded border p-2">Delete</button>
+          </form>
+        </div>
+      </div>
+      <pre className="rounded border border-gray-200 bg-gray-200 p-3">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
--- src/components/snippetEditForm.tsx
+++ src/components/snippetEditForm.tsx
@@ -1,11 +1,45 @@
 'use client';
 
+import { editSnippet } from '@/actions';
+import { Editor } from '@monaco-editor/react';
 import type { Snippet } from '@prisma/client';
+import { useState, useTransition } from 'react';
 
 interface Props {
   snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: Props) {
-  return <div>Editing snippet with title: {snippet.title}</div>;
+  const [code, setCode] = useState(snippet.code);
+  const [isPending, startTransition] = useTransition();
+
+  const handleEditorChange = (value: string = '') => {
+    setCode(value);
+  };
+
+  const handleEditSnippet = () => {
+    startTransition(() => {
+      editSnippet(snippet.id, code);
+    });
+  };
+
+  return (
+    <>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={code}
+        options={{ minimap: { enabled: false }, fontSize: 16 }}
+        onChange={handleEditorChange}
+      />
+      <button
+        className="rounded border p-2"
+        onClick={handleEditSnippet}
+        disabled={isPending}
+      >
+        Save
+      </button>
+    </>
+  );
 }
