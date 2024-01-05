292a3375b3c6ed9c93aa7b843814d0dce0799606
Nicola Erario <nicola.erario@gmail.com>
2024-01-05 08:41:39 +0100

    refactor: Disable cache for snippets fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import db from '@/db';
+import { unstable_noStore as noStore } from 'next/cache';
 import Link from 'next/link';
 
 export default async function Home() {
+  // Disable cache
+  noStore();
+
   const snippets = await db.snippet.findMany();
 
   return (
